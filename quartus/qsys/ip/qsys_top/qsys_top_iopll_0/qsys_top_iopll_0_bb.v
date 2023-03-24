c6adc84a649b651b77d696c97608721ec78439fe
Kevin Weldon <kevin.weldon@intel.com>
2023-03-24 08:38:11 -0400

    Commit

--- quartus/qsys/ip/qsys_top/qsys_top_iopll_0/qsys_top_iopll_0_bb.v
+++
@@ -1,8 +0,0 @@
-module qsys_top_iopll_0 (
-		input  wire  refclk,   //  refclk.clk
-		output wire  locked,   //  locked.export
-		input  wire  rst,      //   reset.reset
-		output wire  outclk_0  // outclk0.clk
-	);
-endmodule
-
